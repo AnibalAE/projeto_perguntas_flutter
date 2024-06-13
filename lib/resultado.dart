c0d1da5558e159904699cd6cf6e8211712992760
AnibalFilho <anibalfilho@pactosolucoes.com.br>
2024-06-12 22:11:14 -0300

    ajuste numa string

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -14,7 +14,7 @@ class Resultado extends StatelessWidget {
     } else if (pontuacao < 16) {
       return 'Impressionante!';
     } else {
-      return 'Nível Jedi!';
+      return 'Nível Jedi!!';
     }
   }
 
